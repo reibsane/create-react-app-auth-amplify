7456e5cd22fab9a8cf2f2a50359d04267baad38a
Alex Reibsane <48532057+reibsane@users.noreply.github.com>
2020-12-18 10:49:50 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,26 +5,63 @@ import { withAuthenticator } from 'aws-amplify-react'
 import Amplify, { Auth } from 'aws-amplify';
 import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+
+
+import Menu from './Menu/Menu';
+import Hero from './Hero/Hero';
+import HomePage from './HomePage/HomePage';
+import Footer from './Footer/Footer';
+import LoginPage from './LoginPage/LoginPage';
+import Logout from './Logout/Logout';
+import Signup from './Signup/Signup';
+import Dashboard from './Dashboard/Dashboard';
+import Chart from './Charts/Chart';
+import Chart2 from './Charts/Chart2';
+import Chart3 from './Charts/Chart3';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <Router>
+      <Menu/>
+      <Hero/>
+      <div className="mainContainer">
+        <Switch>
+
+          <Route path="/login">
+            <LoginPage/>
+          </Route>
+
+          <Route path="/logout">
+            <Logout/>
+          </Route>
+
+          <Route path="/signup">
+            <Signup/>
+          </Route>
+
+          <Route path="/dashboard">
+            <Dashboard/>
+            <Chart/>
+            <Chart2/>
+            <Chart3/>
+          </Route>
+
+          <Route path="/">
+            <HomePage/>
+          </Route>
+
+        </Switch>
       </div>
+      
+      <Footer/>
+    </Router>
     );
   }
 }
